bb8be231e9f92a64bd228069da700ba988e7f303
Hermansyah <43913036+0dayherman@users.noreply.github.com>
2019-08-21 21:34:09 +0700

    Update ip.php

--- idx/ip.php
+++ idx/ip.php
@@ -1,5 +1,6 @@
 <?php
 $ip = readline("IP : ");
+echo "Reverse IP..\n\n";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.indoxploit.or.id/ip/".$ip);
 curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTMLlike Gecko) Chrome/72.0.3626.109 Safari/537.36');
@@ -8,8 +9,8 @@ $output = curl_exec($ch);
 $dec = json_decode($output);
 foreach($dec->data->resolutions as $key){
 print_r($key->hostname."\n");
-$save = @fopen("revip.txt", "a");
-fwrite($save, $key->hostname."\n");
-fclose($save);
+// $save = @fopen("revip.txt", "a");
+// fwrite($save, $key->hostname."\n");
+// fclose($save);
 }
 ?>
